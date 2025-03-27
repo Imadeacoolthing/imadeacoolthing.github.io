9caa9d6d546ee2788625625b1c4c58f416f46fd4
NotPickle <noahuang1@gmail.com>
2025-03-27 11:24:48 -0400

    PicklePicklePickle

--- BalatroWeb/Jokerdata.js
+++ BalatroWeb/Jokerdata.js
@@ -13,20 +13,36 @@ let Jokers = {
       Name: "Greedy Joker",
       Desc: "Played cards with  Diamond suit give +3 Mult when scored",
       Id: 2,
+      Cost: 5,
+      Rare: 1,
     },
     Lustly_J: {
         Name: "Lustly Joker",
         Desc: "Played cards with  Heart suit give +3 Mult when scored",
         Id: 3,
+        Cost: 5,
+        Rare: 1,
     },
     Wrathful_J: {
       Name: "Wrathful Joker",
       Desc: "Played cards with  Spade suit give +3 Mult when scored",
       Id: 4,
+      Cost: 5,
+      Rare: 1,
     },
     Gluttonous_J: {
      Name: "Gluttonous Joker",
      Desc: "Played cards with  Club suit give +3 Mult when scored",
      Id: 5,
+     Cost: 5,
+     Rare: 1,
     },
+    Jolly_J: {
+      Name: "Jolly Joker",
+      Desc: "+8 Mult if played hand contains a Pair",
+      Id: 6,
+      Cost: 3,
+      Rare: 1,
+    },
+    
   }
