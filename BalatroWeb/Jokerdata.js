184b9538a2432e9acda65a49f8551fea0d4d92a4
Imadeacoolthing <yoshimaniac20@gmail.com>
2025-03-27 07:33:12 -0400

    bean

---
+++ BalatroWeb/Jokerdata.js
@@ -0,0 +1,18 @@
+// Add , after every line
+let Jokers = {
+    Joker: {
+      Name: "Joker",
+      Desc: "+4 Mult",
+      Id: 1,
+    },
+    Greedy_J: {
+      Name: "Greedy Joker",
+      Desc: "Played cards with  Diamond suit give +3 Mult when scored",
+      Id: 2,
+    },
+    Lustly_J: {
+        Name: "Lustly Joker",
+        Desc: "Played cards with  Heart suit give +3 Mult when scored",
+        Id: 3,
+    },
+  }
---
+++ JOBS.md
@@ -0,0 +1,3 @@
+Linus:
+Inside of the BalatroWeb folder, there is a file called "Jokerdata.js" fill these out using the chart on this website: https://imadeacoolthing.github.io/jk.html
+
