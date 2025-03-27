0de141c8cd49d8696fa8d84c317df496658e2a91
Imadeacoolthing <yoshimaniac20@gmail.com>
2025-03-27 11:33:17 -0400

    beans

---
+++ BalatroWeb/Blinddata.js
@@ -0,0 +1,14 @@
+// Add commas after every line
+let blinds = {
+    Small_blind: {
+        Name: "Small Blind",
+        // How many times base
+        Req: 1,
+        // The first ante it shows up on
+        Minimum_Ante: 1,
+        // Amnt of money earned
+        Money_Earned: 3,
+        // The number it is on the chart
+        Id: 1,
+    },
+}
