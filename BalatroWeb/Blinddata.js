abe4c360471f0c0bf9d2ed14028e7adfaf95dd27
Imadeacoolthing <yoshimaniac20@gmail.com>
2025-03-28 11:04:21 -0400

    bean

--- BalatroWeb/Blinddata.js
+++ BalatroWeb/Blinddata.js
@@ -2,13 +2,9 @@
 let blinds = {
     Small_blind: {
         Name: "Small Blind",
-    1x
         Req: 1,
-    1
         Minimum_Ante: 1,
-    $3
         Money_Earned: 3,
-    1
         Id: 1,
     },
 }
