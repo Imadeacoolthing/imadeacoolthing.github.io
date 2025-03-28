ef46e2125818f8b12fc56efef6e6f4aee38a1c49
SquiddyMcPie <whittlinus@gmail.com>
2025-03-28 11:21:17 -0400

    Update Blinddata.js

--- BalatroWeb/Blinddata.js
+++ BalatroWeb/Blinddata.js
@@ -6,5 +6,31 @@ let blinds = {
         Minimum_Ante: 1,
         Money_Earned: 3,
         Id: 1,
+        Desc: "No special effects - can be skipped to receive a Tag"
     },
+    Big_blind: {
+        Name: "Big Blind",
+        Req: 1.5,
+        Minimum_Ante: 1,
+        Money_Earned: 4,
+        Id: 2,
+        Desc: "No special effects - can be skipped to receive a Tag"
+    }
+    The_Hook: {
+        Name: "The Hook",
+        Req: 2
+        Minimun_Ante: 1
+        Money_Earned: 5
+        Id: 3
+        Desc: "Discards 2 random cards held in hand after every played hand",
+}
+    The_Ox: {
+        Name: "The Ox"
+        Req: 2
+        Minimum_Ante: 6
+        Money_Earned: 5
+        Id: 4
+    }
+
+    
 }
